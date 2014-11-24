aa0dd2a2997c96cb9a012db516ba55fd03016a04
Phil Johnston <support@mintplugins.com>
2014-11-24 12:05:27 -0500

    Better check for extending the user's session

--- includes/misc-functions/extend-user-session.php
+++ includes/misc-functions/extend-user-session.php
@@ -23,24 +23,23 @@
  * @return   void
  */
 function mp_restrict_logins_extend_session( $user_id ){
-	
-	
+
 	//If we're not doing ajax
-	if (!defined('DOING_AJAX')){	
+	//if (!defined('DOING_AJAX')){	
 		
 		//Re Set "transient" to tell system that user is still logged in and 'holding' this account for another 31 seconds (make sure it's double your heartbeat tick).
 		//Think of this as "Kicking the can down the road".	Like the US debt, it'll crash eventually, but not right now.	
-		set_transient( 'mp_restrict_logins_user_timeout_' . $user_id, time() + 31, 24 * HOUR_IN_SECONDS );
+		set_transient( 'mp_restrict_logins_user_timeout_' . $user_id, time() + 300, 24 * HOUR_IN_SECONDS );
 		
 		//Set "session" upon user sign in - expires 31 seconds from the current time.	
-		$_SESSION['mp_restrict_logins_user_session_timeout_' . $user_id] = time() + 31;
+		$_SESSION['mp_restrict_logins_user_session_timeout_' . $user_id] = time() + 300;
 		
-		/* Let's keep this here for testing purposes when we need it 			
-		echo 'Transient extended to ' . get_transient( 'mp_restrict_logins_user_timeout_' . $user_id) . ' for user ' . $user_id;
-		echo '<br />';
-		echo 'Session extended to ' . $_SESSION['mp_restrict_logins_user_session_timeout_' . $user_id];
+		/* Let's keep this here for testing purposes when we need it 	*/		
+		//echo 'Transient extended to ' . get_transient( 'mp_restrict_logins_user_timeout_' . $user_id) . ' for user ' . $user_id;
+		//echo '<br />';
+		//echo 'Session extended to ' . $_SESSION['mp_restrict_logins_user_session_timeout_' . $user_id];
 		//*/				
-	}
+	//}
 						
 	
 }
